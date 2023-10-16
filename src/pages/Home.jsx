76b98fdeefbdd5296d85230b8f496a9c020848ab
Cesar <tu@email.com>
2023-10-16 17:55:44 -0300

    SE CAMBIO EL TITULO DEL HOME

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 import Banner from "../components/Banner/Banner";
 import BannerImage from "../assets/banners/banner.png";
 import ProductCarousel from "../components/ProductCarousel/ProductCarousel";
-import ServiceSection from '../components/ServiceSection/ServiceSection';
-import ContactSection from '../components/ContactSection/ContactSection';
+import ServiceSection from "../components/ServiceSection/ServiceSection";
+import ContactSection from "../components/ContactSection/ContactSection";
 import { getMotorcycles } from "../services/motorcycles";
 
 export default () => {
+  const [motorcycles, setMotorcycles] = useState([]);
 
-    const [motorcycles, setMotorcycles] = useState([]);
+  const fetchMotorcycles = async () => {
+    const motorcyclesData = await getMotorcycles();
+    setMotorcycles(motorcyclesData);
+  };
 
-    const fetchMotorcycles = async () => {
-        const motorcyclesData = await getMotorcycles();
-        setMotorcycles(motorcyclesData);
-    }
+  useEffect(() => {
+    fetchMotorcycles();
+  }, []);
 
-    useEffect(() => {
-        fetchMotorcycles();
-    }, [])
-
-    return (
-        <>
-            <Banner
-                image={BannerImage}
-                title="MOTO CANDELA"
-                subtitle="Guarenas"
-            />
-            <ProductCarousel motorcycles={motorcycles} />
-            <ServiceSection />
-            <ContactSection />
-        </>
-    );
-}
+  return (
+    <>
+      <Banner
+        image={BannerImage}
+        title="MOTO CANDELA LAS LACRAS"
+        subtitle="Guarenas"
+      />
+      <ProductCarousel motorcycles={motorcycles} />
+      <ServiceSection />
+      <ContactSection />
+    </>
+  );
+};
